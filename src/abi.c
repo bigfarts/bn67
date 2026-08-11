f21cb9c8ad27fc3853cdcf8eefaa8392271d1e4a
bigfarts <bigfarts@punymail.com>
2026-08-10 19:34:35 -0700

    fix folderback

--- src/abi.c
+++ src/abi.c
@@ -84,6 +84,7 @@ Exe6Runtime *exe6_runtime(void)
 }
 
 NATIVE_WRAPPER(exe6_sound_req, 0x080005CC, void, (uint32_t sound))
+NATIVE_WRAPPER(exe6_get_cur_pet_navi, 0x080010B6, uint32_t, (void))
 NATIVE_WRAPPER(exe6_mem_task_trans_set256, 0x08000AC8, void, (const void *source, void *destination, uint32_t size))
 NATIVE_WRAPPER_R4(exe6_col_fade_set, 0x08002378, void, (uint32_t unused, uint32_t color, uint32_t count, uint32_t cache, uintptr_t destination))
 NATIVE_WRAPPER(exe6_col_fade_kill, 0x0800239A, void, (uint32_t cache))
@@ -161,6 +162,30 @@ NATIVE_WRAPPER(
 )
 NATIVE_WRAPPER(exe6_navi_status_set, 0x080136B0, void, (uint32_t side, uint32_t property, uint32_t value))
 NATIVE_WRAPPER(exe6_navi_status_get, 0x080136CC, uint32_t, (uint32_t side, uint32_t property))
+NATIVE_WRAPPER(
+    exe6_set_cur_pet_navi_stats_byte,
+    0x0801379E,
+    void,
+    (uint32_t navi, uint32_t property, uint32_t value)
+)
+NATIVE_WRAPPER(
+    exe6_get_cur_pet_navi_stats_byte,
+    0x080137B6,
+    uint32_t,
+    (uint32_t navi, uint32_t property)
+)
+NATIVE_WRAPPER(
+    exe6_set_special_navi_stats_byte,
+    0x08013864,
+    void,
+    (uint32_t unused, uint32_t property, uint32_t value)
+)
+NATIVE_WRAPPER(
+    exe6_get_special_navi_stats_byte,
+    0x08013884,
+    uint32_t,
+    (uint32_t unused, uint32_t property)
+)
 NATIVE_WRAPPER(exe6_battle_key_work_adrs_get, 0x0800A0F4, const uint8_t *, (uint32_t side))
 NATIVE_WRAPPER(exe6_cockpit_set_custom_gauge_value, 0x0801DFA2, void, (uint32_t value))
 NATIVE_WRAPPER(exe6_cockpit_pause_set, 0x0801E15C, void, (void))
--- src/abi.h
+++ src/abi.h
@@ -1066,10 +1066,29 @@ void exe6_battle_report_flag_on(uint32_t control_flags);
 void exe6_battle_report_flag_off(uint32_t control_flags);
 
 Exe6Obj *exe6_get_navi_adrs(uint32_t side);
+uint32_t exe6_get_cur_pet_navi(void);
 Exe6NaviStatusWork *exe6_navi_status_work_adrs_get(uint32_t side);
 uint32_t exe6_navi_status_get(uint32_t side, uint32_t property);
 void exe6_navi_status_set(
     uint32_t side,
+    uint32_t property,
+    uint32_t value
+);
+uint32_t exe6_get_cur_pet_navi_stats_byte(
+    uint32_t navi,
+    uint32_t property
+);
+void exe6_set_cur_pet_navi_stats_byte(
+    uint32_t navi,
+    uint32_t property,
+    uint32_t value
+);
+uint32_t exe6_get_special_navi_stats_byte(
+    uint32_t unused,
+    uint32_t property
+);
+void exe6_set_special_navi_stats_byte(
+    uint32_t unused,
     uint32_t property,
     uint32_t value
 );
